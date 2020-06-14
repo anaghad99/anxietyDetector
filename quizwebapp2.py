c251562b7ceed14e4f17583da4bd533ece529f81
anaghad99 <anaghad99@gmail.com>
2020-06-13 20:33:09 -0700

    Add files via upload
    code for short quiz to diagnose anxiety

---
+++ quizwebapp2.py
@@ -0,0 +1,441 @@
+
+Level_One = 0
+Level_Two = 0
+Level_Three = 0
+Level_Four = 0
+
+
+print("Hello! My name is Helper. I will be guiding you through this quiz. Please do not think that I am a medical professional, because I am not!"
+      " If my results strike you as incorrect, please feel free to do the things recommended or ignore my diagnosis.Note: For the letters, please"
+      " put in the exact lowercase letter that represents the phrase that is next to it. (e.g. a, not A)")
+
+print("How often do you feel nervous, anxious, or on edge?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q1_answer = input("Please enter the corresponding letter:")
+
+if q1_answer == 'a':
+    Level_One += 1
+elif q1_answer == 'b':
+    Level_Two += 1
+elif q1_answer == 'c':
+    Level_Three +=  1
+elif q1_answer == 'd':
+    Level_Four += 1
+
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(Level_One)
+print(Level_Two)
+print(Level_Three)
+print(Level_Four)
+print(" ")
+
+print("How often do you worry uncontrollably?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q2_answer = input("Please enter the corresponding letter:")
+
+if q2_answer == 'a':
+    Level_One += 1
+elif q2_answer == 'b':
+    Level_Two += 1
+elif q2_answer == 'c':
+    Level_Three += 1
+elif q2_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("When do you typically have trouble relaxing?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q3_answer = input("Please enter the corresponding letter:")
+
+if q3_answer == 'a':
+    Level_One += 1
+elif q3_answer == 'b':
+    Level_Two += 1
+elif q3_answer == 'c':
+    Level_Three += 1
+elif q3_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("When do you typically feel restless(not sitting still)?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q4_answer = input("Please enter the corresponding letter:")
+
+if q4_answer == 'a':
+    Level_One += 1
+elif q4_answer == 'b':
+    Level_Two += 1
+elif q4_answer == 'c':
+    Level_Three += 1
+elif q4_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever become easily annoyed or irritable?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q5_answer = input("Please enter the corresponding letter:")
+
+if q5_answer == 'a':
+    Level_One += 1
+elif q5_answer == 'b':
+    Level_Two += 1
+elif q5_answer == 'c':
+    Level_Three += 1
+elif q5_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever feel overwhelmingly afraid?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q6_answer = input("Please enter the corresponding letter:")
+
+if q6_answer == 'a':
+    Level_One += 1
+elif q6_answer == 'b':
+    Level_Two += 1
+elif q6_answer == 'c':
+    Level_Three += 1
+elif q6_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever get headaches at school?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q7_answer = input("Please enter the corresponding letter:")
+
+if q7_answer == 'a':
+    Level_One += 1
+elif q7_answer == 'b':
+    Level_Two += 1
+elif q7_answer == 'c':
+    Level_Three += 1
+elif q7_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever worry about not being as good as other kids?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q8_answer = input("Please enter the corresponding letter:")
+
+if q8_answer == 'a':
+    Level_One += 1
+elif q8_answer == 'b':
+    Level_Two += 1
+elif q8_answer == 'c':
+    Level_Three += 1
+elif q8_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever get a nauseous sensation when scared?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q9_answer = input("Please enter the corresponding letter:")
+
+if q9_answer == 'a':
+    Level_One += 1
+elif q9_answer == 'b':
+    Level_Two += 1
+elif q9_answer == 'c':
+    Level_Three += 1
+elif q9_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever worry about things that already occured?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q10_answer = input("Please enter the corresponding letter:")
+
+if q10_answer == 'a':
+    Level_One += 1
+elif q10_answer == 'b':
+    Level_Two += 1
+elif q10_answer == 'c':
+    Level_Three += 1
+elif q10_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you ever get nervous about being watched?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q11_answer = input("Please enter the corresponding letter:")
+
+if q11_answer == 'a':
+    Level_One += 1
+elif q11_answer == 'b':
+    Level_Two += 1
+elif q11_answer == 'c':
+    Level_Three += 1
+elif q11_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("When you are nervous, do your palms sweat unncessarily?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q12_answer = input("Please enter the corresponding letter:")
+
+if q12_answer == 'a':
+    Level_One += 1
+elif q12_answer == 'b':
+    Level_Two += 1
+elif q12_answer == 'c':
+    Level_Three += 1
+elif q12_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("Do you worry about sleeping alone?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q13_answer = input("Please enter the corresponding letter:")
+
+if q13_answer == 'a':
+    Level_One += 1
+elif q13_answer == 'b':
+    Level_Two += 1
+elif q13_answer == 'c':
+    Level_Three += 1
+elif q13_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print("When you are nervous, do you shake uncontrollably?")
+print(" ")
+print("a)never")
+print("b)several days a month")
+print("c)more than half the month")
+print("d)all the time")
+q14_answer = input("Please enter the corresponding letter:")
+
+if q14_answer == 'a':
+    Level_One += 1
+elif q14_answer == 'b':
+    Level_Two += 1
+elif q14_answer == 'c':
+    Level_Three += 1
+elif q14_answer == 'd':
+    Level_Four += 1
+else:
+    print("Sorry, that response cannot be accepted. Try again")
+
+print(" ")
+
+print(Level_One)
+print(Level_Two)
+print(Level_Three)
+print(Level_Four)
+    
+if Level_One > Level_Two and Level_One > Level_Three and Level_One > Level_Four:
+    print("You do not have anxiety, you are doing perfectly fine!In case you think there is something wrong, or you want to maintain good mental health, please do the following:")
+    print(" ")
+    print("1) Monitor your mental health regularly (https://screening.mhanational.org/screening-tools")
+    print("2) Learn/Read up about ways to stay healthy (https://www.mhanational.org/live-your-life-well")
+    print("3) Read up on some common symptoms (https://www.mhanational.org/b4stage4-get-informed")
+    print("4) Get tips on your mental health (https://www.mhanational.org/31-tips-boost-your-mental-health")
+    print("5) Text HOME to 741741 to connect with a Crisis")
+    print("Thanks for taking the quiz, and please listen to the feedback!")
+
+elif Level_Two > Level_One and Level_Two > Level_Three and Level_Two > Level_Four:
+    print("It seems that you have a little bit of anxiety. Please do the following to help:")
+    print(" ")
+    print("1) Take a mental health screen, take the result and show them to the doctor.(https://screening.mhanational.org/screening-tools)")
+    print("2) Find a therapist (https://www.mhanational.org/finding-therapy)")
+    print("3) Find a support group (https://www.mhanational.org/find-support-groups)")
+    print("4) Learn more (https://www.mhanational.org/MentalHealthInfo)")
+    print("5) Communication Worksheets (https://www.mhanational.org/sites/default/files/Worksheet%20-%20Whats%20Underneath.pdf)")
+    print("6) Text HOME to 741741 to connect with a Crisis")
+    print(" ")
+    print("Thanks for taking this quiz, and please listen to the feedback!")
+elif Level_Three > Level_One and Level_Three > Level_Two and Level_Three > Level_Four:
+    print("It seems that your anxiety is getting worse. Please do the following to help on your recovery:")
+    print(" ")
+    print("1) Call 1-800-273-TALK or text 741741 if you are in a crisis")
+    print("2) Look up your local affiliate for services (https://arc.mhanational.org/find-an-affiliate)")
+    print("3) Find a therapist (https://www.mhanational.org/finding-therapy)")
+    print("4) Find a support group (https://www.mhanational.org/find-support-groups)")
+    print("5) Plan for a crisis (https://www.mhanational.org/psychiatric-advance-directive)")
+    print(" ")
+    print("Thanks for taking this quiz, and please listen to the feedback!")    
+elif Level_Four > Level_One and Level_Four > Level_Two and Level_Four > Level_Three:
+    print("It seems that you are in a crisis, and are struggling with your anxiety. Not to worry, these steps will help you recover quickly:")
+    print(" ")
+    print("1) Call 1-800-273-TALK or text 741741 if you are in a crisis")
+    print("2) Find a local affiliate for services (https://arc.mhanational.org/find-an-affiliate)")
+    print("3) Find a therapist (https://www.mhanational.org/finding-therapy)")
+    print("4) Find a support group (https://www.mhanational.org/find-support-groups)")
+    print("5) Plan for a crisis (https://www.mhanational.org/psychiatric-advance-directive)")
+    print(" ")
+    print("Thanks for taking this quiz, and please listen to the feedback!")
+else:
+    print("Too difficult to decide... Maybe this quiz needs more questions..")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
